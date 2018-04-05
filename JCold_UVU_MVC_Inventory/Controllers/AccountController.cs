24f06d53d240ddc8cd5bb84825c0b1d8fc47cea3
j_j_c@hotmail.com <36175703+joshzcold@users.noreply.github.com>
2018-04-05 10:55:45 -0600

    Fixed registering

--- JCold_UVU_MVC_Inventory/Controllers/AccountController.cs
+++ JCold_UVU_MVC_Inventory/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace JCold_UVU_MVC_Inventory.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, BirthDate = model.BirthDate, UserRoleId = 1 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
